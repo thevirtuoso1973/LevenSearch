ec59c59c3d6532b3b4965cf2a825daf89ab0e087
thevirtuoso1973 <chris.tomy@hotmail.com>
2021-03-13 00:58:32 +0000

    implement dynamic programming based levenshtein

--- content_scripts/levensearch.js
+++ content_scripts/levensearch.js
@@ -9,19 +9,66 @@
     }
     window.hasRun = true;
 
-    /**
-     * Given a URL to a beast image, remove all existing beasts, then
-     * create and style an IMG node pointing to
-     * that image, then insert the node into the document.
+    function min(a, b, c) {
+        if (a < b) {
+            return a < c ? a : c;
+        }
+        return b < c ? b : c;
+    }
+
+    /*
+     * computes the levenshtein distance between a,b
      */
+    function lev(a, b) {
+        let d = [];
+        for (let i = 0; i <= a.length; ++i) {
+            let currRow = [];
+            for (let j = 0; j <= b.length; ++j) {
+                currRow.push(0);
+            }
+            d.push(currRow);
+        }
+
+        for (let i = 1; i <= a.length; ++i) {
+            d[i][0] = i;
+        }
+        for (let j = 1; j <= b.length; ++j) {
+            d[0][j] = j;
+        }
+
+        for (let i = 1; i <= a.length; ++i) {
+            for (let j = 1; j <= b.length; ++j) {
+                let cost = a[i-1] === b[j-1] ? 0 : 1;
+                d[i][j] = min(d[i-1][j]+1,
+                              d[i][j-1]+1,
+                              d[i-1][j-1] + cost);
+            }
+        }
+        return d[a.length][b.length]
+    }
+
+    function getTextElements() {
+        return Array.from(document.body.getElementsByTagName("*"))
+            .filter(e => e.childElementCount == 0 && e.textContent.length > 0)
+    }
+
     function levenSearch(query, maxDist) {
         resetQuery();
 
         window.alert("Search: " + query + ", dist: " + maxDist);
+
+        let elements = getTextElements();
+
+        elements.forEach(e => {
+            if (lev(e.textContent, query) <= maxDist) {
+                console.log(e.textContent);
+            }
+        });
     }
 
     function resetQuery() {
         // TODO
+        console.log("Reset button.");
     }
 
     /**
